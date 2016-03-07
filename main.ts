792a5e2fa36207e24f78bd959d91d87bd7f7795b
48089748z <48089748z@iespoblenou.org>
2016-03-07 17:28:08 +0100

    Testing walls.

--- main.ts
+++ main.ts
@@ -12,20 +12,38 @@ class mainState extends Phaser.State {
         this.load.image('ufo', 'assets/UFO_low.png');
         this.load.image('pickup', 'assets/Pickup_low.png');
         this.load.image('background', 'assets/Background_low.png');
-
+        this.load.image('0.0', 'assets/Background_low-0-0.png');
+        this.load.image('0.1', 'assets/Background_low-0-1.png');
+        this.load.image('0.2', 'assets/Background_low-0-2.png');
+        this.load.image('1.0', 'assets/Background_low-1-0.png');
+        this.load.image('1.1', 'assets/Background_low-1-1.png');
+        this.load.image('1.2', 'assets/Background_low-1-2.png');
+        this.load.image('2.0', 'assets/Background_low-2-0.png');
+        this.load.image('2.1', 'assets/Background_low-2-1.png');
+        this.load.image('2.2', 'assets/Background_low-2-2.png');
         this.physics.startSystem(Phaser.Physics.ARCADE);
     }
 
     create():void {
         super.create();
-        var background;
-
-        background = this.add.sprite(0, 0, 'background');
-        var scale = this.world.height / background.height;
-        background.scale.setTo(scale, scale);
-
+        //var background;
         this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
-        this.ufo.scale.setTo(scale - 0.05, scale - 0.05);
+        var wall_00 = this.add.sprite(0, 0, '0.0');
+        var wall_01  = this.add.sprite(0, wall_00.height, '0.1');
+        var wall_02  = this.add.sprite(0, wall_00.height+wall_01.height, '0.2');
+
+      //  var wall_10  = this.add.sprite(0, 0, '1.0');
+       // var wall_11  = this.add.sprite(0, 0, '1.1');
+       // var wall_12  = this.add.sprite(0, 0, '1.2');
+       // var wall_20  = this.add.sprite(0, wall_20.width, '2.0');
+       // var wall_21  = this.add.sprite(wall_20.width, wall_21.width, '2.1');
+       // var wall_22  = this.add.sprite(wall_20.width+wall_21.width, wall2, '2.2');
+
+       // var scale = this.world.height / background.height;
+      //  background.scale.setTo(scale, scale);
+
+
+       // this.ufo.scale.setTo(scale - 0.05, scale - 0.05);
         this.ufo.anchor.setTo(0.5, 0.5);
         this.physics.enable(this.ufo);
         this.cursor = this.input.keyboard.createCursorKeys();
